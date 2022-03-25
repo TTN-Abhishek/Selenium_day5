78a1d412c1d609b3ef9be48953c1f06e5f2901e0
TTN-Abhishek <99463209+TTN-Abhishek@users.noreply.github.com>
2022-03-25 12:09:39 +0530

    Update Que_1.java

--- Selenium_day5/src/Que_1.java
+++ Selenium_day5/src/Que_1.java
@@ -1,41 +1,32 @@
-import java.util.Iterator;
-import java.util.Set;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Que_1 {
+	public static void main(String[] args){
+		//Count using Arraylist
+		ArrayList<String> names = new ArrayList<String>();
+		names.add("Abhishek");
+		names.add("Dikesh");
+		names.add("Devsh");
+		names.add("Debram");
+		names.add("Rahul");
+		System.out.println(names);
 
-	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Abhishek Kumar\\Downloads\\Drivers_Selenium\\chromedriver_win32\\chromedriver.exe");
-		//WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
-
-		driver.manage().window().maximize();
-		Thread.sleep(1000);
-
-		driver.get("http://the-internet.herokuapp.com");
-		Thread.sleep(1000);
-
-		JavascriptExecutor js =(JavascriptExecutor) driver;
-		js.executeScript("window.scrollBy(0,500)");
-
-		Thread.sleep(1000);
-		driver.findElement(By.xpath("//a[contains(text(),'Multiple Windows')]")).click();
-
-		Thread.sleep(1000);
-		driver.findElement(By.xpath("//a[contains(text(),'Click Here')]")).click();
-
-		Set<String> windows = driver.getWindowHandles();
-		Iterator<String> itr = windows.iterator();
-		String parent = itr.next();
-		String child = itr.next();
-		driver.switchTo().window(child);
-		
-		System.out.println(driver.findElement(By.xpath("//h3[contains(text(),'New Window')]")).getText());
-		Thread.sleep(2000);
-		
-		driver.switchTo().window(parent);
-		System.out.println(driver.findElement(By.xpath("//h3[contains(text(),'Opening a new window')]")).getText());
+		long count = 0;
+		for(String i : names) {
+			if(i.startsWith("D"))
+				count++;
+			System.out.println(i);
+		}
+		System.out.println();
+		System.out.println("Count: " + count);
+		//Count using Stream
+		List<String> strList = Arrays.asList("Abhishek" ,"Dikesh" , "Devsh" , "Debram", "Rahul");
+		long count1 = strList.stream()
+				.filter(x -> x.startsWith("D"))
+				.count();
+		System.out.println("Count: " + count1);
 	}
 }
